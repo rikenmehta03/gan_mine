43e36f4333e8022061f34da20f1b03cf844b9c0d
Mudit Pandey <muditpandey@gmail.com>
2018-11-03 01:40:33 -0400

    [ADD] Logger complete with imsave. Train modified. Minor Bug Fixes.

--- gan/gan_trainer.py
+++ gan/gan_trainer.py
@@ -5,10 +5,10 @@ import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.autograd import Variable
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 class GanTrainer():
-    def __init__(self, discriminator, generator, d_optimizer, g_optimizer, d_loss, g_loss, logger):
+    def __init__(self, discriminator, generator, d_optimizer, g_optimizer, d_loss, g_loss, logger, device = torch.device('cpu')):
+        self.device = device
         self.generator = generator
         self.discriminator = discriminator
         self.d_optimizer = d_optimizer
@@ -16,27 +16,27 @@ class GanTrainer():
         self.d_loss = d_loss
         self.g_loss = g_loss
         self.logger = logger
-        self.test_noise = self._noise(32)
+        self.test_noise = self._noise(32) # Input: No. of noise samples
     
     def _noise(self, num_samples):
         '''
         Generates a 1-d vector of gaussian sampled random values
         '''
-        n = Variable(torch.randn(num_samples, self.generator.noise_size, device=device)) # add noise_size as class variable of generator
+        n = torch.randn(num_samples, self.generator.noise_size, 1, 1, device = self.device) # add noise_size as class variable of generator
         return n
     
     def _ones_target(self, size):
         '''
         Tensor containing ones, with shape = size
         '''
-        data = Variable(torch.ones(size, 1, device=device))
+        data = torch.ones(size, 1, device = self.device)
         return data
 
     def _zeros_target(self, size):
         '''
         Tensor containing zeros, with shape = size
         '''
-        data = Variable(torch.zeros(size, 1, device=device))
+        data = torch.zeros(size, 1, device = self.device)
         return data
     
     def _train_discriminator(self, real_data, fake_data):
@@ -81,49 +81,79 @@ class GanTrainer():
         # Return error
         return error
         
-    def train(self, data_loader, num_epochs, batch_size, verbose = 1, checkpoint=False):
-        for epoch in range(num_epochs):
-            d_total_error, g_total_error = 0.0, 0.0
-            total_pred_real, total_pred_fake = 0, 0
-            for n_batch, (real_batch,_) in enumerate(data_loader):
-                N = real_batch.size(0)
+    def _train(self, epoch, data_loader, verbose):
+        d_total_error, g_total_error = 0.0, 0.0
+        total_pred_real, total_pred_fake = 0, 0
+        for batch_idx, (real_batch,_) in enumerate(data_loader):
+            N = real_batch.size(0)
 
-                # 1. Train Discriminator
-                real_data = Variable(real_batch).to(device)
+            # 1. Train Discriminator
+            real_data = real_batch.to(self.device)
 
-                # Generate fake data and detach 
-                # (so gradients are not calculated for generator)
-                fake_data = self.generator(self._noise(N)).detach().to(device)
+            # Generate fake data and detach 
+            # (so gradients are not calculated for generator)
+            fake_data = self.generator(self._noise(N)).detach().to(self.device)
 
-                # Train D
-                d_error, d_pred_real, d_pred_fake =  self._train_discriminator(real_data, fake_data)
+            # Train D
+            d_error, d_pred_real, d_pred_fake =  self._train_discriminator(real_data, fake_data)
 
-                # 2. Train Generator
+            # 2. Train Generator
 
-                # Generate fake data
-                fake_data = self.generator(self._noise(N)).to(device)
+            # Generate fake data
+            fake_data = self.generator(self._noise(N)).to(self.device)
 
-                # Train G
-                g_error = self._train_generator(fake_data)
+            # Train G
+            g_error = self._train_generator(fake_data)
 
-                d_total_error += d_error
-                g_total_error += g_error
-                total_pred_real += d_pred_real.sum()
-                total_pred_fake += d_pred_fake.sum()
+            d_total_error += d_error
+            g_total_error += g_error
+            total_pred_real += d_pred_real.sum()
+            total_pred_fake += d_pred_fake.sum()
 
-            test_images = self.generator(self.test_noise)
+            if verbose > 0:
+                self.logger.print_progress(batch_idx + 1,
+                    len(data_loader),
+                    prefix = 'Train Epoch: {}'.format(epoch),
+                    suffix = 'DLoss: {:.6f} GLoss: {:.6f}'.format(d_error.item(),g_error.item()),
+                    bar_length = 50)
+            break
 
-            d_total_error = (d_total_error * batch_size)/(2 * len(data_loader.dataset))
-            g_total_error = (g_total_error * batch_size)/(len(data_loader.dataset))
-            total_pred_fake /= (1.0 * len(data_loader.dataset))
-            total_pred_real /= (1.0 * len(data_loader.dataset)) 
-            
-            state = {
-                'epoch': epoch,
-                'd_error': d_total_error,
-                'g_error': g_total_error,
-                'd_pred_real': total_pred_real,
-                'd_pred_fake': total_pred_fake,
-                'test_images': test_images
-            }
+        
+        #--------------------------------------------------------------------------------------------
+        # Training Ends.
+        # -------------------------------------------------------------------------------------------
+        
+        # Generate test images after training for each epoch
+
+        test_images = self.generator(self.test_noise)
+
+        # Logging details.
+
+        d_total_error = (d_total_error * data_loader.batch_size)/(2 * len(data_loader.dataset))
+        g_total_error = (g_total_error * data_loader.batch_size)/(len(data_loader.dataset))
+        total_pred_fake /= (1.0 * len(data_loader.dataset))
+        total_pred_real /= (1.0 * len(data_loader.dataset))
+
+        if verbose > 0:
+            line = '------------------ Epoch: {} ------------------\n'.format(epoch)
+            line += "Discriminator Average Error: {:.6f} , Generator Average Error: {:.6f}\n".format(d_total_error,g_total_error)
+            line += 'D(x): {:.4f}, D(G(z)): {:.4f}\n'.format(total_pred_real,total_pred_fake)
+            print(line)
+        
+        state = {
+            'epoch': epoch,
+            'd_error': d_total_error,
+            'g_error': g_total_error,
+            'd_pred_real': total_pred_real,
+            'd_pred_fake': total_pred_fake,
+            'test_images': test_images
+        }
+        return state
+        #self.logger.log(self, state)
+
+    def trainer(self, data_loader, num_epochs, verbose = 1, checkpoint=False):
+        for epoch in range(1, num_epochs+1):
+            state = self._train(epoch, data_loader, verbose)
             self.logger.log(self, state)
+
+
--- utils/logger.py
+++ utils/logger.py
@@ -1,37 +1,65 @@
 import os
+import sys
 import time 
 import torchvision.utils as vutils
 
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 
 class Logger():
-    def __init__(self, file_name_prefix = ''):
+    def __init__(self, file_name_prefix = None):
         time_stamp = time.strftime("%d_%m_%Y_%H:%M:%S", time.localtime())
+        if file_name_prefix is not None:
+            time_stamp = file_name_prefix + '_' + time_stamp
         self.dir_name = os.path.join(os.path.dirname(DIR_PATH), 'logs', time_stamp)
         
-        if os.path.exists(self.dir_name):
+        if not os.path.exists(self.dir_name):
             os.makedirs(self.dir_name)
         
-        self.log_file = open(os.path.join(self.dir_name, 'logfile.log'), 'w')
-        
+        self.log_file_name = os.path.join(self.dir_name, 'logfile.log')
+
         self.g_loss_array = []
         self.d_loss_array = []
 
     def _log_text(self, state):
         d_g_loss = 'Discriminator Loss: {:.4f}, Generator Loss: {:.4f}\n'.format(state['d_error'], state['g_error'])
         d_g_acc = 'D(x): {:.4f}, D(G(z)): {:.4f}\n'.format(state['d_pred_real'], state['d_pred_fake'])
-        self.log_file.write('------------------ Epoch: {} ------------------\n'.format(state['epoch']))
-        self.log_file.write(d_g_loss)
-        self.log_file.write(d_g_acc)
+        with open(self.log_file_name, 'a') as fp:
+            fp.write('------------------ Epoch: {} ------------------\n'.format(state['epoch']))
+            fp.write(d_g_loss)
+            fp.write(d_g_acc)
 
     def _log_output_images(self, state, normalize=True):
         images = state['test_images']
         _grid = vutils.make_grid(images, normalize=normalize, scale_each=True)
-        
+        image_name = os.path.join(self.dir_name, str(state['epoch']) +'_GIMG.png')
+        vutils.save_image(_grid,image_name) 
+
     def log(self, gan_object, state):
         self.g_loss_array.append(state['d_error'])
         self.d_loss_array.append(state['g_error'])
         
         self._log_text(state)
         self._log_output_images(state)
-        
+    
+    def print_progress(self, iteration, total, prefix='', suffix='', decimals=1, bar_length=100):
+        """
+        Call in a loop to create terminal progress bar
+        @params:
+            iteration   - Required  : current iteration (Int)
+            total       - Required  : total iterations (Int)
+            prefix      - Optional  : prefix string (Str)
+            suffix      - Optional  : suffix string (Str)
+            decimals    - Optional  : positive number of decimals in percent complete (Int)
+            bar_length  - Optional  : character length of bar (Int)
+        """
+        str_format = "{0:." + str(decimals) + "f}"
+        percents = str_format.format(100 * (iteration / float(total)))
+        filled_length = int(round(bar_length * iteration / float(total)))
+        bar = '█' * filled_length + '-' * (bar_length - filled_length)
+
+        sys.stdout.write('\r%s |%s| %s%s %s' % (prefix, bar, percents, '%', suffix)),
+
+        if iteration == total:
+            sys.stdout.write('\n')
+        sys.stdout.flush()
+
