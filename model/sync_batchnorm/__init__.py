a60c17d8e6739e88eded94592377e75c89e442e2
Unknown <riken.mehta03@gmail.com>
2018-12-08 15:36:22 -0500

    [UPDATE]normal batch norm

--- model/sync_batchnorm/__init__.py
+++ model/sync_batchnorm/__init__.py
@@ -8,5 +8,6 @@
 # https://github.com/vacancy/Synchronized-BatchNorm-PyTorch
 # Distributed under MIT License.
 
-from .batchnorm import SynchronizedBatchNorm1d, SynchronizedBatchNorm2d, SynchronizedBatchNorm3d
+# from .batchnorm import SynchronizedBatchNorm1d, SynchronizedBatchNorm2d, SynchronizedBatchNorm3d
+from .bnorm import SynchronizedBatchNorm2d
 from .replicate import DataParallelWithCallback, patch_replication_callback
