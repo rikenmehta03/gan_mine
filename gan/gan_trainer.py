66699f252bd8cecb126d0a157d2a5f61a4bfd1e7
Unknown <riken.mehta03@gmail.com>
2018-12-14 06:48:32 -0500

    [UPDATE]trainer to use iters

--- gan/gan_trainer.py
+++ gan/gan_trainer.py
@@ -1,5 +1,6 @@
+import os, sys
 import time
-import os
+import datetime
 import errno
 import torch
 import torch.nn as nn
@@ -8,9 +9,8 @@ import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.autograd import Variable
 
-
 class GanTrainer():
-    def __init__(self, discriminator, generator, d_optimizer, g_optimizer, d_loss, g_loss, logger, test_size = None, resume = None, device = torch.device('cpu')):
+    def __init__(self, discriminator, generator, d_optimizer, g_optimizer, d_loss, g_loss, logger, log_iter=1000, test_size = None, resume = None, device = torch.device('cpu')):
         self.device = device
         self.generator = generator
         self.discriminator = discriminator
@@ -21,12 +21,14 @@ class GanTrainer():
         self.d_loss = d_loss
         self.g_loss = g_loss
         self.logger = logger
-        self.start_epoch = 1
+        self.log_iter = log_iter
         self.iter = 1
+        self.noise_size = self.generator.in_ch
         if test_size:
-            self.test_noise = self._noise(test_size) # Input: No. of noise samples
+            self.test_noise = torch.randn(test_size, self.noise_size).to(self.device) # Input: No. of noise samples
         else:
             self.test_noise = None
+
         if resume is not None:
             if os.path.exists(resume):
                 self._load_checkpoint(resume)
@@ -45,173 +47,99 @@ class GanTrainer():
         Input: File path
         '''
         checkpoint = torch.load(resume)
-        self.start_epoch = checkpoint['epoch'] + 1
         self.iter = checkpoint['iter']
         self.generator.load_state_dict(checkpoint['g_state'])
         self.discriminator.load_state_dict(checkpoint['d_state'])
         self.g_optimizer.load_state_dict(checkpoint['g_optimizer'])
         self.d_optimizer.load_state_dict(checkpoint['d_optimizer'])
-        print("loaded checkpoint {} (Epoch {})".format(resume, checkpoint['epoch']))
-
-    def _noise(self, num_samples):
-        '''
-        Generates a 1-d vector of gaussian sampled random values
-        '''
-        n = torch.randn(num_samples, self.generator.in_ch, 1, 1, device = self.device) 
-        return n
-    
-    def _ones_target(self, size):
-        '''
-        Tensor containing ones, with shape = size
-        '''
-        data = torch.ones(size, 1, device = self.device)
-        return data
-
-    def _zeros_target(self, size):
-        '''
-        Tensor containing zeros, with shape = size
-        '''
-        data = torch.zeros(size, 1, device = self.device)
-        return data
-    
-    def _train_discriminator(self, real_data, fake_data):
-        N = real_data.size(0)
-        # Reset gradients
-        self.d_optimizer.zero_grad()
-        
-        # 1.1 Train on Real Data
-        prediction_real = self.discriminator(real_data)
-        # Calculate error and backpropagate
-        error_real = self.d_loss(prediction_real, self._ones_target(N))
-        error_real.backward()
-
-        # 1.2 Train on Fake Data
-        prediction_fake = self.discriminator(fake_data)
-        # Calculate error and backpropagate
-        error_fake = self.d_loss(prediction_fake, self._zeros_target(N))
-        error_fake.backward()
-        
-        # 1.3 Update weights with gradients
-        self.d_optimizer.step()
-        
-        # Return error and predictions for real and fake inputs
-        return error_real + error_fake, prediction_real, prediction_fake
-    
-    def _train_generator(self, fake_data):
-        N = fake_data.size(0)
-
-        # Reset gradients
-        self.g_optimizer.zero_grad()
-
-        # Sample noise and generate fake data
-        prediction = self.discriminator(fake_data)
-
-        # Calculate error and backpropagate
-        error = self.g_loss(prediction, self._ones_target(N))
-        error.backward()
-
-        # Update weights with gradients
-        self.g_optimizer.step()
-
-        # Return error
-        return error
+        print("loaded checkpoint {} (Iter {})".format(resume, checkpoint['iter']))
     
     def _denorm(self, x):
         out = (x + 1) / 2
         return out.clamp_(0, 1)
+    
+    def reset_grad(self):
+        self.d_optimizer.zero_grad()
+        self.g_optimizer.zero_grad()
+    
+    def trainer(self, data_loader, num_iter, verbose = 1):
+        self.batch_size = data_loader.batch_size
+        if self.test_noise is None:
+            self.test_noise = torch.randn(self.batch_size, self.generator.in_ch).to(self.device)
         
-    def _train(self, epoch, data_loader, verbose):
+        self.data_loader = data_loader
+        
+        data_iter = iter(self.data_loader)
         start_time = time.time()
-        d_total_error, g_total_error = 0.0, 0.0
-        total_pred_real, total_pred_fake = 0, 0
-        for batch_idx, (real_batch,_) in enumerate(data_loader):
-            N = real_batch.size(0)
+        start_iter = self.iter
+        ones = torch.ones(self.batch_size, 1).to(self.device)
+        zeros = torch.zeros(self.batch_size, 1).to(self.device)
 
-            # 1. Train Discriminator
+        for batch_idx in range(start_iter, num_iter):
+            self.generator.train()
+            self.discriminator.train()
+            
+            try:
+                real_batch, _ = next(data_iter)
+            except:
+                data_iter = iter(self.data_loader)
+                real_batch, _ = next(data_iter)
+
             real_data = real_batch.to(self.device)
 
-            # Generate fake data and detach 
-            # (so gradients are not calculated for generator)
-            fake_data = self.generator(self._noise(N)).detach().to(self.device)
+            # Train D    
+            z = torch.randn(self.batch_size, self.noise_size).to(self.device)        
+            fake_data = self.generator(z).detach().to(self.device)
 
-            # Train D
-            d_error, d_pred_real, d_pred_fake =  self._train_discriminator(real_data, fake_data)
+            self.reset_grad()
 
-            # 2. Train Generator
+            d_out_real = self.discriminator(real_data)
+            d_loss_real = self.d_loss(d_out_real, ones)
+            d_loss_real.backward()
+            D_x = d_out_real.mean().item()
 
-            # Generate fake data
-            fake_data = self.generator(self._noise(N)).to(self.device)
+            d_out_fake = self.discriminator(fake_data)
+            d_loss_fake = self.d_loss(d_out_fake, zeros)
+            d_loss_fake.backward()
+            D_G_z1 = d_out_fake.mean().item()
 
-            # Train G
-            g_error = self._train_generator(fake_data)
+            d_loss = d_loss_real + d_loss_fake
 
-            d_total_error += d_error
-            g_total_error += g_error
-            total_pred_real += d_pred_real.sum()
-            total_pred_fake += d_pred_fake.sum()
+            self.d_optimizer.step()
+
+            # ================== Train G ================== #
+            g_out_fake = self.discriminator(fake_data)  # batch x n
+            g_loss_fake = self.g_loss(g_out_fake, ones)
+            D_G_z2 = g_out_fake.mean().item()
+
+            self.reset_grad()
+            g_loss_fake.backward()
+            self.g_optimizer.step()
 
             if verbose > 0:
-                self.logger.print_progress(batch_idx + 1,
-                    len(data_loader),
-                    prefix = 'Train Epoch: {} Iter: {}'.format(epoch, self.iter),
-                    suffix = 'DLoss: {:.6f} GLoss: {:.6f}'.format(d_error.item(),g_error.item()),
+                elapsed = time.time() - start_time
+                elapsed = str(datetime.timedelta(seconds=elapsed))
+                self.logger.print_progress((batch_idx) % len(self.data_loader)+1,
+                    len(self.data_loader),
+                    prefix = 'Train Iter: {}/{}'.format(self.iter, num_iter),
+                    suffix = 'DLoss: {:.6f} GLoss: {:.6f} Elapsed: {}'.format(d_loss.item(),g_loss_fake.item(),elapsed),
                     bar_length = 50)
             
             if verbose > 0 and self.iter % 100 == 0:
-                # Generate test images after training for each epoch
-                self.generator.eval()
-                test_images = self.generator(self.test_noise)
-                self.generator.train()
-                # Logging details.
+                test_images = self.generator(self.test_noise).detach()
                 t_del = time.time() - start_time
-                line = '------------------ Iter: {} ------------------\n'.format(self.iter)
-                line += "Discriminator Average Error: {:.6f} , Generator Average Error: {:.6f}\n".format(d_total_error/200.0,g_total_error/100.0)
-                line += 'D(x): {:.4f}, D(G(z)): {:.4f}, Time: {:.8f}\n'.format(total_pred_real/(100.0 * data_loader.batch_size),total_pred_fake/(100.0 * data_loader.batch_size), t_del)
-                self.logger.log_iter(self, self.iter, line, self._denorm(test_images), normalize=True)
-                d_total_error, g_total_error = 0.0, 0.0
-                total_pred_real, total_pred_fake = 0, 0
+                line = 'Item: {}, Time: {:.8f}\n'.format(self.iter, t_del)
+                self.logger.log_iter(self, self.iter, line, self._denorm(test_images.data), normalize=True)
+                del test_images
+
+            if self.iter % self.log_iter == 0:
+                state = {
+                    'iter': self.iter,
+                    'd_state': self.discriminator.state_dict(),
+                    'g_state': self.generator.state_dict(),
+                    'd_optimizer': self.d_optimizer.state_dict(),
+                    'g_optimizer': self.g_optimizer.state_dict()
+                }
+                self.logger.log_epoch(self, state)
             
             self.iter += 1
-
-        
-        #--------------------------------------------------------------------------------------------
-        # Training Ends.
-        # -------------------------------------------------------------------------------------------
-        
-        # # Generate test images after training for each epoch
-        # self.generator.eval()
-        # test_images = self.generator(self.test_noise)
-        # self.generator.train()
-        # # Logging details.
-
-        # d_total_error = (d_total_error * data_loader.batch_size)/(2 * len(data_loader.dataset))
-        # g_total_error = (g_total_error * data_loader.batch_size)/(len(data_loader.dataset))
-        # total_pred_fake /= (1.0 * len(data_loader.dataset))
-        # total_pred_real /= (1.0 * len(data_loader.dataset))
-        # t_del = time.time() - start_time
-        # if verbose > 0:
-        #     line = '------------------ Epoch: {} ------------------\n'.format(epoch)
-        #     line += "Discriminator Average Error: {:.6f} , Generator Average Error: {:.6f}\n".format(d_total_error,g_total_error)
-        #     line += 'D(x): {:.4f}, D(G(z)): {:.4f}, Time: {:.8f}\n'.format(total_pred_real,total_pred_fake, t_del)
-        #     print(line)
-        
-        state = {
-            'epoch': epoch,
-            'iter': self.iter,
-            'd_error': d_total_error,
-            'g_error': g_total_error,
-            'd_pred_real': total_pred_real,
-            'd_pred_fake': total_pred_fake,
-            'd_state': self.discriminator.state_dict(),
-            'g_state': self.generator.state_dict(),
-            'd_optimizer': self.d_optimizer.state_dict(),
-            'g_optimizer': self.g_optimizer.state_dict()
-        }
-        return state
-
-    def trainer(self, data_loader, num_epochs, verbose = 1, checkpoint=False):
-        if self.test_noise is None:
-            self.test_noise = self._noise(data_loader.batch_size)
-        for epoch in range(self.start_epoch, num_epochs+1):
-            state = self._train(epoch, data_loader, verbose)
-            self.logger.log_epoch(self, state)
